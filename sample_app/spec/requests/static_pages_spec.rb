b221d1c764391f5e86b71b52832c105a228a1e44
hfeng-air <harrifeng@gmail.com>
2013-09-21 15:53:29 +0800

    pages are all ready, test need DRY

--- sample_app/spec/requests/static_pages_spec.rb
+++ sample_app/spec/requests/static_pages_spec.rb
@@ -2,35 +2,47 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Sample App')
+    it "should have the content 'Home'" do
+      visit root_path
+      expect(page).to have_content('Home')
     end
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_title("Sample App | Home")
     end
   end
   
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the title 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("Sample App | Help")
     end
   end
   
   describe "About page" do
     it "should have the content 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About Us')
     end
     it "should have the title 'About'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("Sample App | About Us")
     end
-  end    
+  end
+  
+  describe "Contact page" do
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Sample App | Contact")
+    end
+  end
+  
 end
